0358e54eb0f794ce3655a63e14fd32335214327b
Tran Ngoc Hau <22520412@gm.uit.edu.vn>
2025-04-28 15:17:11 +0700

    Re-add w3storage-upload-script as a normal folder

---
+++ w3storage-upload-script/generateKey.js
@@ -0,0 +1,9 @@
+﻿import crypto from 'crypto';
+
+// create AES-256 key(32 byte)
+const aesSecretKey = crypto.randomBytes(32).toString('hex');
+console.log('AES Secret Key:', aesSecretKey);
+
+// create HMAC key (32 byte)
+const hmacSecretKey = crypto.randomBytes(32).toString('hex');
+console.log('HMAC Secret Key:', hmacSecretKey);
